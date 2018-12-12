cdcd496314d46ba86abdc444a0a023fa748ca4ee
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2018-12-12 16:27:12 +0700

    Initial commit

---
+++ app/src/main/java/rifafauzi6/id/simplempvconcept/MainActivity.java
@@ -0,0 +1,13 @@
+package rifafauzi6.id.simplempvconcept;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
